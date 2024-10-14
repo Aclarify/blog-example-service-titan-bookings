b92f261b0b256f6b1b53f95d41f58b12026e9e10
Spencer Marx <spencer.s.marx@gmail.com>
2024-10-14 12:53:04 +0200

    chore: extend booking types for outbound DTO for ServiceTitan

--- src/lib/types/booking.type.ts
+++ src/lib/types/booking.type.ts
@@ -1,3 +1,26 @@
+export type Address = {
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  country: string;
+};
+
+export type Contact = {
+  type: "Phone" | "Email";
+  value: string;
+};
+
+export interface CreateBookingOutboundDto {
+  source: string;
+  name: string;
+  summary: string;
+  isFirstTimeClient: boolean;
+  externalId: string;
+  address: Address;
+  contacts: Contact[];
+}
+
 export type CreateBookingFormDto = {
   firstName: string;
   lastName: string;
